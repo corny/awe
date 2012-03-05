5aa038c2f16bf5220d60be8b46dbeedf7530c148
henning mueller <henning@orgizm.net>
2012-03-05 11:05:23 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awemgmt::Application.config.secret_token = '1b02388bc6f0487a3f854e4117aefe48c13be87b46ff424e5008f7915a4aa77f08ac35f4ae5783f547fad1eb2f3f3e163d01483397d00da7ffc93233db2ae292'
