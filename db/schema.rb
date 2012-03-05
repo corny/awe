896cee2705019e6cc1773ab76b6c6d010a890ae5
Julian Kornberger <juliank@tzi.de>
2012-03-05 14:33:03 +0100

    migrations merged

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120305113214) do
+ActiveRecord::Schema.define(:version => 20120305101942) do
 
   create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email",      :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "name"
-    t.string   "email"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
