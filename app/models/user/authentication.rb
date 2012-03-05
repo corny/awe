4e7c407e87843b8b51c5d9879393e1b639f78762
Julian Kornberger <juliank@tzi.de>
2012-03-05 23:22:46 +0100

    deprecation warning removed

--- app/models/user/authentication.rb
+++ app/models/user/authentication.rb
@@ -25,28 +25,26 @@ module User::Authentication
     end
   end
   
-  module InstanceMethods
-    def openid_fields=(fields)
-      logger.debug fields
-  
-      fields.each do |key, value|
-        # Some AX providers can return multiple values per key
-        if value.is_a? Array
-          value = value.first
-        end
-  
-        case key.to_s
-          when 'http://openid.tzi.de/spec/schema/displayName'
-            self.name = value
-          when 'http://openid.tzi.de/spec/schema/mail'
-            self.email = value
-          when 'http://openid.tzi.de/spec/schema/degreeProgram'
-            self.degree_program = value
-          when 'http://openid.tzi.de/spec/schema/matriculationNumber'
-            self.matriculation = value.to_i
-          else
-            logger.error "Unknown OpenID field: #{key}"
-        end
+  def openid_fields=(fields)
+    logger.debug fields
+
+    fields.each do |key, value|
+      # Some AX providers can return multiple values per key
+      if value.is_a? Array
+        value = value.first
+      end
+
+      case key.to_s
+        when 'http://openid.tzi.de/spec/schema/displayName'
+          self.name = value
+        when 'http://openid.tzi.de/spec/schema/mail'
+          self.email = value
+        when 'http://openid.tzi.de/spec/schema/degreeProgram'
+          self.degree_program = value
+        when 'http://openid.tzi.de/spec/schema/matriculationNumber'
+          self.matriculation = value.to_i
+        else
+          logger.error "Unknown OpenID field: #{key}"
       end
     end
   end
